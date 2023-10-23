e11b51c5f60649a1f10b2381581eb8dda7894fc5
christinatel <61830068+christinatel@users.noreply.github.com>
2023-10-23 10:51:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function lukk(){
     rules = document.getElementById("rules");
     rules.style.display="none";
 }
+
+
+ const suits = ["Spar", "Clover", "Ruter", "Hearts"];
+
+document.getElementById("revealCard").addEventListener("click", function() {
+    const revealedSuit = suits[Math.floor(Math.random() * suits.length)];
+    document.getElementById("revealedSuit").textContent = revealedSuit;
+    document.getElementById("revealedSuit").style.display = "block";
+});
